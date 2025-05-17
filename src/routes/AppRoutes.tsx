555f7c343219f1d61b7f31c1c5ca4b54ba5683c7
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-05-16 20:37:03 -0700

    created notfound page.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from '../layouts/AppLayout';
-import Home from '../components/Home';
+import Home from '../pages/Home';
+import NotFoundPage from '../pages/NotFoundPage';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<AppLayout />}>
+        <Route path="/" element={<AppLayout />}>
           <Route index element={<Home />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
