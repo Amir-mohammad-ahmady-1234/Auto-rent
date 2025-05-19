bee0cab9857e09877c59ada1f11c2fe97c74f83a
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-05-18 20:37:05 -0700

    responsice BannerSection and CartListHeader.

--- src/components/Home/BannerSection.tsx
+++ src/components/Home/BannerSection.tsx
@@ -1,17 +1,17 @@
 const BannerSection = () => {
   return (
- <div className="relative flex min-h-[396px] w-full flex-col overflow-hidden rounded-2xl pb-[104px] md:max-w-full md:pb-[100px]">
-          <img
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/0c2ace1309830c3dde3d54305d1dcc35fdc5b0c6?placeholderIfAbsent=true"
-            className="absolute inset-0 h-full w-full object-cover object-center"
-            alt="Banner background"
-          />
-          <img
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/492585bc045a54ff78aa0865bdcf30f21a1368c2?placeholderIfAbsent=true"
-            className="mb-[-21px] aspect-[4.18] w-full object-contain object-center md:mb-[10px] md:max-w-full"
-            alt="Banner content"
-          />
-        </div>
+    <div className="relative flex min-h-[200px] w-full flex-col items-center justify-center overflow-hidden rounded-2xl md:min-h-[396px] md:max-w-full">
+      <img
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/0c2ace1309830c3dde3d54305d1dcc35fdc5b0c6?placeholderIfAbsent=true"
+        className="absolute inset-0 h-full w-full object-contain object-center"
+        alt="Banner background"
+      />
+      {/* <img
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/492585bc045a54ff78aa0865bdcf30f21a1368c2?placeholderIfAbsent=true"
+        className="absolute bottom-0 left-0 z-10 aspect-[4.18] w-full max-w-full object-contain object-center px-4"
+        alt="Banner content"
+      /> */}
+    </div>
   );
 };
 
