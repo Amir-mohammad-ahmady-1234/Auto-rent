4b3da8a1463eed62f9497053528bbf4cda597517
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-06-13 13:27:12 -0700

    responsive login page.

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -6,9 +6,9 @@ const LoginLevelOne = () => {
       dir="ltr"
       className="flex h-screen w-full flex-col items-center bg-gray-200"
     >
-      <div className="m-auto flex h-6/7 w-3/5 flex-row overflow-hidden rounded-2xl bg-white shadow-lg">
+      <div className="m-auto flex h-full w-full max-w-screen-lg flex-col overflow-hidden rounded-2xl bg-white shadow-lg md:h-6/7 md:w-4/5 md:flex-row">
         {/* Image section */}
-        <div className="w-1/3">
+        <div className="hidden shrink-0 md:block md:h-full md:w-2/8">
           <img
             src="images/olav-tvedt--oVaYMgBMbs-unsplash 1.png"
             alt="car"
@@ -17,19 +17,19 @@ const LoginLevelOne = () => {
         </div>
 
         {/* Form section */}
-        <div className="relative flex flex-1 flex-col justify-start px-10 py-6">
+        <div className="relative flex flex-1 flex-col justify-start px-6 py-6 sm:px-10">
           {/* Logo */}
-          <div className="mt-2 mb-8 self-center">
+          <div className="mt-2 mb-6 self-center">
             <Logo />
           </div>
 
           {/* Content */}
           <div className="flex h-full flex-col justify-center gap-6">
-            <h2 className="text-center text-2xl font-semibold">
+            <h2 className="text-center text-xl font-semibold md:text-2xl">
               ورود / ثبت‌نام
             </h2>
-            <p className="text-center text-gray-600">
-              .کد تأیید به شماره موبایلی که وارد می‌کنید ارسال خواهد شد
+            <p className="text-center text-sm leading-relaxed text-gray-600 md:text-base">
+              کد تأیید به شماره موبایلی که وارد می‌کنید ارسال خواهد شد.
             </p>
 
             <div className="flex flex-col gap-3">
@@ -37,10 +37,10 @@ const LoginLevelOne = () => {
                 id="phone"
                 type="tel"
                 placeholder="شماره موبایل"
-                className="rounded-md border border-gray-300 p-3 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                className="rounded-md border border-gray-300 p-3 text-sm focus:ring-2 focus:ring-blue-500 focus:outline-none md:text-base"
               />
 
-              <label className="flex items-center justify-end gap-2 text-sm">
+              <label className="flex items-center justify-end gap-2 text-right text-sm">
                 <span>
                   با ورود و ثبت‌نام در سایت، با{' '}
                   <span className="text-blue-500">قوانین اتورنت</span> موافقت
@@ -49,7 +49,7 @@ const LoginLevelOne = () => {
                 <input type="checkbox" className="accent-blue-500" />
               </label>
 
-              <button className="rounded-md cursor-pointer bg-blue-600 py-2 text-white transition hover:bg-blue-700">
+              <button className="rounded-md bg-blue-600 py-2 text-sm text-white transition hover:bg-blue-700 md:text-base">
                 تأیید و ادامه
               </button>
             </div>
