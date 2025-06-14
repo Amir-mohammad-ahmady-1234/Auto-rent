6d38d50440237a1fc588acee94da32cb12f140e7
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-06-14 11:43:34 -0700

    added title for ivited cars.

--- src/components/CarRental/InviteCars.tsx
+++ src/components/CarRental/InviteCars.tsx
@@ -1,24 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CarsContext } from '../../context/cars/CarsContext';
 import Car from '../../ui/Car';
 import { Link } from 'react-router-dom';
+import type { TCar } from '../../types/CarType';
 
 const InviteCars = () => {
   const { cars } = useContext(CarsContext);
-  const randomNum1 = Math.round(Math.random() * 5);
-  const randomNum2 = Math.round(Math.random() * 5);
-  const randomNum3 = Math.round(Math.random() * 5);
 
-  const randomCars = cars.filter(
-    (car) =>
-      car.id === randomNum1 || car.id === randomNum2 || car.id === randomNum3
-  );
+  const [randomCars, setRandomCars] = useState<TCar[]>([]);
+
+  useEffect(() => {
+    if (cars.length === 0) return;
+
+    const threeRandomUniqueCars = [...cars]
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 3);
+
+    setRandomCars(threeRandomUniqueCars);
+  }, [cars]);
 
   return (
-    <div className='flex flex-col mr-10'>
-      <Link to="/rent/cars">
-        <div className="my-auto text-[#194BF0]">مشاهده همه{' >'}</div>
-      </Link>
+    <div className="mr-10 flex flex-col">
+      <div className="flex items-center justify-around">
+        <h4>خودرو هایی که شاید دوست داشته باشد ...</h4>
+        <Link to="/rent/cars">
+          <div className="my-auto text-[#194BF0]">مشاهده همه{' >'}</div>
+        </Link>
+      </div>
       <div className="mt-6 px-4 sm:px-2 md:max-w-full">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {randomCars.map((car) => (
