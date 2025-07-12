3a9f246f4cb2237a22c68ed7301708aa949206e8
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-11 17:47:36 -0700

    mutation (post) reservations data.

--- src/components/Payment/Rules accept and pat btn/PayAllManyBtn.tsx
+++ src/components/Payment/Rules accept and pat btn/PayAllManyBtn.tsx
@@ -5,9 +5,10 @@ import { useUserRserveInfo } from '../../../context/userReservedData/useUserRese
 import { formatNumber } from '../../../utils/formatNumber.1';
 import toast from 'react-hot-toast';
 import { useAuth } from '../../../context/Auth/useAuth';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getUserId } from '../../../services/apiAuth';
 import FullPageLoading from '../../../ui/FullPageLoading';
+import { insertReservation } from '../../../services/apiReservePosts';
 
 const PayAllManyBtn = ({ isRulsAccept }: { isRulsAccept: boolean }) => {
   const { totalAmount } = useDepositPrice();
@@ -17,23 +18,91 @@ const PayAllManyBtn = ({ isRulsAccept }: { isRulsAccept: boolean }) => {
   const { formInfo, mainCar } = useReservedInfo();
   const { phone } = useAuth();
 
-  function handlePay() {
-    toast.success('پرداخت شما با موفقیت انجام شد.', {
-      duration: 3000,
-    });
+  const queryClient = useQueryClient();
 
-    setCurrentStep(5);
-  }
-
-  const { data: userID, isLoading } = useQuery<string | null, Error>({
+  const { data: mainUser_id, isLoading } = useQuery<string | null, Error>({
     queryKey: ['users'],
     queryFn: () => getUserId(phone),
   });
 
-  if (isLoading) return <FullPageLoading />;
+  const { data, mutateAsync, error, status } = useMutation({
+    mutationFn: insertReservation,
+    onSuccess: () => {
+      // Invalidate queries to refetch data after successful mutation
+      queryClient.invalidateQueries({ queryKey: ['reservations'] });
+      console.log('Item created successfully!');
+    },
+    onError: (error) => {
+      console.error('Error creating item:', error);
+    },
+  });
 
-  console.log(totalAmount, userReservedInfo, formInfo, mainCar?.id, userID);
-  
+  if (error) throw new Error(error.message);
+
+  if (
+    isLoading ||
+    !userReservedInfo ||
+    !formInfo ||
+    !mainCar ||
+    status === 'pending'
+  )
+    return <FullPageLoading />;
+
+  const {
+    deliveryTime,
+    pickupLocation,
+    rentalDays,
+    rentalType,
+    returnLocation,
+    returnTime,
+    deliveryDate,
+    returnDate,
+  } = formInfo;
+
+  const {
+    adress,
+    email,
+    lastName,
+    name,
+    nationalCode,
+    phone: userPhone,
+  } = userReservedInfo;
+
+  const { id: mainCar_id } = mainCar;
+
+  const newReservation = {
+    deliveryDate: deliveryDate.format(),
+    deliveryTime,
+    rentalType,
+    userPhone: userPhone?.toString() || '',
+    pickupLocation,
+    rentalDays,
+    returnDate: returnDate.format(),
+    returnLocation,
+    returnTime,
+    adress,
+    email,
+    lastName,
+    name,
+    nationalCode: nationalCode?.toString() || '',
+    totalAmount,
+    mainCar_id,
+    mainUser_id: parseInt(mainUser_id as string),
+  };
+
+  async function handlePay() {
+    const req = await mutateAsync(newReservation);
+    toast.success('پرداخت شما با موفقیت انجام شد.', {
+      duration: 3000,
+    });
+    console.log(req);
+    console.log(data);
+    setCurrentStep(5);
+  }
+
+
+  // console.log(totalAmount, userReservedInfo, formInfo, mainCar?.id, userID);
+
   return (
     <>
       {/* Gray payment summary box */}
---
+++ src/services/apiReservePosts.ts
@@ -0,0 +1,30 @@
+import { supabase } from './supabase'; // آدرس فایل کانفیگ supabase
+
+interface Reservation {
+  deliveryDate: string;
+  deliveryTime: string;
+  userPhone: string;
+  pickupLocation: string;
+  rentalDays: number;
+  rentalType: string;
+  returnDate: string;
+  returnLocation: string;
+  returnTime: string;
+  adress: string;
+  email: string;
+  lastName: string;
+  name: string;
+  nationalCode: string;
+  totalAmount: number;
+  mainCar_id: string;
+  mainUser_id: number;
+}
+
+export async function insertReservation(reservation: Reservation) {
+  const { data, error } = await supabase
+    .from('reservations')
+    .insert([reservation]);
+
+  if (error) throw new Error(error.message);
+  return data;
+}
