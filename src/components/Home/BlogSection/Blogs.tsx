19909a10d841f7a2b35a7c8038ea8213a85a1ad9
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-19 13:02:32 -0700

    added some blogs in home page.

--- src/components/Home/BlogSection/Blogs.tsx
+++ src/components/Home/BlogSection/Blogs.tsx
@@ -1,122 +1,29 @@
+import { useFetchBlogs } from '../../../hooks/useBlogQuery';
+import FullPageLoading from '../../../ui/FullPageLoading';
+import MainBlogCards from '../../blogSection/MainBlogCards';
+
 const Blogs = () => {
+  const { blogs: allBlogs, error, isLoading } = useFetchBlogs();
+
+  if (isLoading) return <FullPageLoading />;
+
+  const someBlogs = allBlogs?.slice(0, 3);
+
   return (
-    <div className="mt-6 flex w-full max-w-[1221px] flex-wrap items-center justify-start gap-6 font-[Estedad-FD] md:max-w-full">
-      {/* Blog Card 1 */}
-      <div className="flex-basis-0 my-auto flex min-w-[240px] flex-1 flex-shrink items-start justify-start gap-2 self-stretch overflow-hidden rounded-2xl border border-[#D7D7D7] bg-white p-4">
-        <div className="flex-basis-0 flex w-full min-w-[240px] flex-1 flex-shrink flex-col items-stretch justify-center">
-          <img
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/04b4015d98131e3f6771d20b82fb2ae566c4f258?placeholderIfAbsent=true"
-            className="aspect-[1.84] w-full rounded-xl object-contain object-center"
-            alt="Blog post"
+    <div className="order-2 grid grid-cols-1 gap-6 md:order-1 md:col-span-3 md:grid-cols-2 lg:grid-cols-3">
+      {!error &&
+        someBlogs?.map((item) => (
+          <MainBlogCards
+            content={item.content}
+            id={item.id}
+            readTime={item.readTime}
+            views={item.views}
+            src={item.src}
+            title={item.title}
+            key={item.id}
           />
-          <div className="mt-4 flex w-full flex-col items-stretch justify-center px-1">
-            <div className="flex w-full flex-col items-stretch justify-center">
-              <div className="flex w-full flex-col items-stretch justify-center">
-                <div className="text-right text-base font-medium text-[#414141]">
-                  روش های مختلف برای اجاره خودرو{' '}
-                </div>
-                <div className="-webkit-box-orient-vertical -webkit-line-clamp-2 text-overflow-ellipsis mt-4 text-justify text-xs font-normal text-[#757575]">
-                  امروزه با تغییراتی که در سبک زندگی مردم و شرایط اقتصادی جوامع
-                  به وجود آمده است نیازهای افراد و نحوه دسترسی به آنها نیز دچار
-                  تحولاتی شده است.
-                </div>
-              </div>
-              <div className="mt-4 flex w-full items-center justify-end gap-4 text-right text-sm font-normal whitespace-nowrap text-[#757575] md:whitespace-normal">
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    1,213
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    3دقیقه
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Blog Card 2 */}
-      <div className="flex-basis-0 my-auto flex min-w-[240px] flex-1 flex-shrink items-start justify-start gap-2 self-stretch overflow-hidden rounded-2xl border border-[#D7D7D7] bg-white p-4">
-        <div className="flex-basis-0 flex w-full min-w-[240px] flex-1 flex-shrink flex-col items-stretch justify-center">
-          <img
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/bc7bee1dd87106d4caaa98bd3f15e0ce977f7429?placeholderIfAbsent=true"
-            className="aspect-[1.84] w-full rounded-xl object-contain object-center"
-            alt="Blog post"
-          />
-          <div className="mt-4 flex w-full flex-col items-stretch justify-center px-1">
-            <div className="flex w-full flex-col items-stretch justify-center">
-              <div className="flex w-full flex-col items-stretch justify-center">
-                <div className="text-right text-base font-medium text-[#414141]">
-                  روش های مختلف برای اجاره خودرو{' '}
-                </div>
-                <div className="-webkit-box-orient-vertical -webkit-line-clamp-2 text-overflow-ellipsis mt-4 text-justify text-xs font-normal text-[#757575]">
-                  امروزه با تغییراتی که در سبک زندگی مردم و شرایط اقتصادی جوامع
-                  به وجود آمده است نیازهای افراد و نحوه دسترسی به آنها نیز دچار
-                  تحولاتی شده است.
-                </div>
-              </div>
-              <div className="mt-4 flex w-full items-center justify-end gap-4 text-right text-sm font-normal whitespace-nowrap text-[#757575] md:whitespace-normal">
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    1,213
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    3دقیقه
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Blog Card 3 */}
-      <div className="flex-basis-0 my-auto flex min-w-[240px] flex-1 flex-shrink items-start justify-start gap-2 self-stretch overflow-hidden rounded-2xl border border-[#D7D7D7] bg-white p-4">
-        <div className="flex-basis-0 flex w-full min-w-[240px] flex-1 flex-shrink flex-col items-stretch justify-center">
-          <img
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/8b773999bd11d8aebdb36643c3a16c57ed4aa880?placeholderIfAbsent=true"
-            className="aspect-[1.84] w-full rounded-xl object-contain object-center"
-            alt="Blog post"
-          />
-          <div className="mt-4 flex w-full flex-col items-stretch justify-center px-1">
-            <div className="flex w-full flex-col items-stretch justify-center">
-              <div className="flex w-full flex-col items-stretch justify-center">
-                <div className="text-right text-base font-medium text-[#414141]">
-                  روش های مختلف برای اجاره خودرو{' '}
-                </div>
-                <div className="-webkit-box-orient-vertical -webkit-line-clamp-2 text-overflow-ellipsis mt-4 text-justify text-xs font-normal text-[#757575]">
-                  امروزه با تغییراتی که در سبک زندگی مردم و شرایط اقتصادی جوامع
-                  به وجود آمده است نیازهای افراد و نحوه دسترسی به آنها نیز دچار
-                  تحولاتی شده است.
-                </div>
-              </div>
-              <div className="mt-4 flex w-full items-center justify-end gap-4 text-right text-sm font-normal whitespace-nowrap text-[#757575] md:whitespace-normal">
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    1,213
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-                <div className="my-auto flex items-center justify-center gap-2 self-stretch md:whitespace-normal">
-                  <div className="my-auto self-stretch text-[#757575]">
-                    3دقیقه
-                  </div>
-                  <div className="my-auto flex h-[18px] w-[18px] flex-shrink-0 self-stretch"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        ))}
+      {error && <h1 className="">مشکل در بارگذازی مقالات</h1>}
     </div>
   );
 };
--- src/components/Home/BlogSection/IntroBlogSection.tsx
+++ src/components/Home/BlogSection/IntroBlogSection.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const IntroBlogSection = () => {
   return (
     <div className="mt-12 ml-[23px] flex w-full max-w-[1034px] flex-wrap items-stretch gap-[5px] self-start text-center font-[Estedad-FD] md:mt-10 md:max-w-full">
       <div className="mt-[47px] flex min-h-12 items-center justify-center gap-2 self-end overflow-hidden rounded-lg px-4 py-2 text-lg leading-8 font-semibold text-[#194BF0] capitalize md:mt-10">
         <div className="my-auto flex h-5 w-5 flex-shrink-0 self-stretch"></div>
-        <div className="my-auto self-stretch text-[#194BF0]">مشاهده همه</div>
+        <Link to="/blogs/all_blogs">
+          <div className="my-auto self-stretch text-[#194BF0]">مشاهده همه</div>
+        </Link>
       </div>
       <div className="flex-basis-0 flex w-fit flex-shrink-0 flex-grow flex-col items-center justify-center md:max-w-full">
         <div className="flex w-[845px] max-w-full flex-col items-center justify-center">
