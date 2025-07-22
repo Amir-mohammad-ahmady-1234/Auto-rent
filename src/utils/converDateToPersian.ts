5bdd718cc0a1085588afb5503225ba2e68dcd8e5
Amir-Mohammad <amyrmhmdahmdy179@gmail.com>
2025-07-21 17:19:38 -0700

    refactor past days show for each comment.

--- src/utils/converDateToPersian.ts
+++ src/utils/converDateToPersian.ts
@@ -1,9 +1,21 @@
 export function convertDateToPersian(date: Date) {
-  const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-  const persianDate = new Intl.DateTimeFormat('fa-IR', options).format(date);
-  return persianDate;
+  const today = new Date();
+
+  const oneDay = 24 * 60 * 60 * 1000;
+  const diffDays = Math.floor((today.getTime() - date.getTime()) / oneDay);
+
+  if (diffDays === 0) {
+    return 'امروز';
+  } else if (diffDays === 1) {
+    return 'دیروز';
+  } else if (diffDays > 1) {
+    return `${diffDays} روز پیش`;
+  } else {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return new Intl.DateTimeFormat('fa-IR', options).format(date);
+  }
 }
